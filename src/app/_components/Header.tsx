c186df6d9ef931f860d72eb3a0e560602ead3bc3
yukiuyeda <yueda940408@gmail.com>
2024-08-18 21:13:26 +0900

    ログイン状態の取得の実装

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,13 +1,36 @@
 import React from "react";
 import Link from "next/link";
+import { useSupabaseSession } from "../_hooks/useSupabaseSession";
+import { supabase } from "@/utils/supabase";
 
 const Header: React.FC = () => {
+const handleLogout = async() => {
+  await supabase.auth.signOut()
+  window.location.href = '/'
+}
+
+  const { session, isLoading } = useSupabaseSession();
+
   return (
     <header className="bg-[#333] text-white p-6 flex justify-between">
-      <Link href="/">
-        Blog
-      </Link>
-      <Link href="/contactform">お問い合わせ</Link>
+      <Link href="/">Blog</Link>
+      {!isLoading && (
+        <>
+          {session ? (
+            <>
+              <Link href="/admin" className="mr-5">管理画面</Link>
+              <button onClick={handleLogout}>ログアウト</button>
+            </>
+          ) : (
+            <>
+              <Link href="/contactform" className="mr-5">
+                お問い合わせ
+              </Link>
+              <Link href="/login">ログイン</Link>
+            </>
+          )}
+        </>
+      )}
     </header>
   );
 };
